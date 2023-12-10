fd84f7c1428b66c719bbb963e7f7cdf295bc354a
Ken <ken3lliot@gmail.com>
2023-12-10 18:28:54 +0700

    Product Tampilan

--- projectpad/projectpad/FormProduct.Designer.cs
+++ projectpad/projectpad/FormProduct.Designer.cs
@@ -34,7 +34,31 @@
             this.buttonClear = new System.Windows.Forms.Button();
             this.buttonDelete = new System.Windows.Forms.Button();
             this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonBack = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.comboBoxMakeFlag = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxGoodsFlag = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDownSafety = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numericUpDownReorder = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.numericUpDownPrice = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.numericUpDownStandard = new System.Windows.Forms.NumericUpDown();
+            this.label8 = new System.Windows.Forms.Label();
+            this.numericUpDownDays = new System.Windows.Forms.NumericUpDown();
+            this.label9 = new System.Windows.Forms.Label();
+            this.dateTimePickerSell = new System.Windows.Forms.DateTimePicker();
+            this.label10 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSafety)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownReorder)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPrice)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownStandard)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDays)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -97,11 +121,197 @@
             this.buttonUpdate.Text = "Update";
             this.buttonUpdate.UseVisualStyleBackColor = true;
             // 
+            // buttonBack
+            // 
+            this.buttonBack.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBack.Location = new System.Drawing.Point(705, 9);
+            this.buttonBack.Name = "buttonBack";
+            this.buttonBack.Size = new System.Drawing.Size(83, 29);
+            this.buttonBack.TabIndex = 29;
+            this.buttonBack.Text = "Back";
+            this.buttonBack.UseVisualStyleBackColor = true;
+            this.buttonBack.Click += new System.EventHandler(this.buttonBack_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 355);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(146, 20);
+            this.label2.TabIndex = 30;
+            this.label2.Text = "Product Name : ";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(164, 355);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(188, 22);
+            this.textBoxName.TabIndex = 31;
+            // 
+            // comboBoxMakeFlag
+            // 
+            this.comboBoxMakeFlag.FormattingEnabled = true;
+            this.comboBoxMakeFlag.Location = new System.Drawing.Point(164, 391);
+            this.comboBoxMakeFlag.Name = "comboBoxMakeFlag";
+            this.comboBoxMakeFlag.Size = new System.Drawing.Size(188, 24);
+            this.comboBoxMakeFlag.TabIndex = 33;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 391);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 20);
+            this.label3.TabIndex = 32;
+            this.label3.Text = "Manufacture : ";
+            // 
+            // comboBoxGoodsFlag
+            // 
+            this.comboBoxGoodsFlag.FormattingEnabled = true;
+            this.comboBoxGoodsFlag.Location = new System.Drawing.Point(164, 429);
+            this.comboBoxGoodsFlag.Name = "comboBoxGoodsFlag";
+            this.comboBoxGoodsFlag.Size = new System.Drawing.Size(188, 24);
+            this.comboBoxGoodsFlag.TabIndex = 35;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 429);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 20);
+            this.label4.TabIndex = 34;
+            this.label4.Text = "Goods Type : ";
+            // 
+            // numericUpDownSafety
+            // 
+            this.numericUpDownSafety.Location = new System.Drawing.Point(202, 471);
+            this.numericUpDownSafety.Name = "numericUpDownSafety";
+            this.numericUpDownSafety.Size = new System.Drawing.Size(150, 22);
+            this.numericUpDownSafety.TabIndex = 37;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 473);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(184, 20);
+            this.label6.TabIndex = 36;
+            this.label6.Text = "Safety Stock Level : ";
+            // 
+            // numericUpDownReorder
+            // 
+            this.numericUpDownReorder.Location = new System.Drawing.Point(202, 508);
+            this.numericUpDownReorder.Name = "numericUpDownReorder";
+            this.numericUpDownReorder.Size = new System.Drawing.Size(150, 22);
+            this.numericUpDownReorder.TabIndex = 39;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 510);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(143, 20);
+            this.label5.TabIndex = 38;
+            this.label5.Text = "Reorder Point : ";
+            // 
+            // numericUpDownPrice
+            // 
+            this.numericUpDownPrice.Location = new System.Drawing.Point(618, 392);
+            this.numericUpDownPrice.Name = "numericUpDownPrice";
+            this.numericUpDownPrice.Size = new System.Drawing.Size(150, 22);
+            this.numericUpDownPrice.TabIndex = 43;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(428, 394);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(109, 20);
+            this.label7.TabIndex = 42;
+            this.label7.Text = "List Price : ";
+            // 
+            // numericUpDownStandard
+            // 
+            this.numericUpDownStandard.Location = new System.Drawing.Point(618, 355);
+            this.numericUpDownStandard.Name = "numericUpDownStandard";
+            this.numericUpDownStandard.Size = new System.Drawing.Size(150, 22);
+            this.numericUpDownStandard.TabIndex = 41;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(428, 357);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(147, 20);
+            this.label8.TabIndex = 40;
+            this.label8.Text = "Standard Cost : ";
+            // 
+            // numericUpDownDays
+            // 
+            this.numericUpDownDays.Location = new System.Drawing.Point(617, 431);
+            this.numericUpDownDays.Name = "numericUpDownDays";
+            this.numericUpDownDays.Size = new System.Drawing.Size(150, 22);
+            this.numericUpDownDays.TabIndex = 45;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(404, 433);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(207, 20);
+            this.label9.TabIndex = 44;
+            this.label9.Text = "Days To Manufacture : ";
+            // 
+            // dateTimePickerSell
+            // 
+            this.dateTimePickerSell.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePickerSell.Location = new System.Drawing.Point(617, 468);
+            this.dateTimePickerSell.Name = "dateTimePickerSell";
+            this.dateTimePickerSell.Size = new System.Drawing.Size(150, 22);
+            this.dateTimePickerSell.TabIndex = 47;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(459, 470);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(152, 20);
+            this.label10.TabIndex = 46;
+            this.label10.Text = "Sell Start Date : ";
+            // 
             // FormProduct
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 542);
+            this.Controls.Add(this.dateTimePickerSell);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.numericUpDownDays);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.numericUpDownPrice);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.numericUpDownStandard);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.numericUpDownReorder);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numericUpDownSafety);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBoxGoodsFlag);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBoxMakeFlag);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonBack);
             this.Controls.Add(this.buttonUpdate);
             this.Controls.Add(this.buttonDelete);
             this.Controls.Add(this.buttonClear);
@@ -112,6 +322,11 @@
             this.Text = "FormProduct";
             this.Load += new System.EventHandler(this.FormProduct_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSafety)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownReorder)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownPrice)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownStandard)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownDays)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -125,5 +340,24 @@
         private System.Windows.Forms.Button buttonClear;
         private System.Windows.Forms.Button buttonDelete;
         private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.ComboBox comboBoxMakeFlag;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxGoodsFlag;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDownSafety;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numericUpDownReorder;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.NumericUpDown numericUpDownPrice;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.NumericUpDown numericUpDownStandard;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown numericUpDownDays;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DateTimePicker dateTimePickerSell;
+        private System.Windows.Forms.Label label10;
     }
 }
--- projectpad/projectpad/FormProduct.cs
+++ projectpad/projectpad/FormProduct.cs
@@ -12,6 +12,7 @@ namespace projectpad
 {
     public partial class FormProduct : Form
     {
+        AdventureWorks2019Entities db = new AdventureWorks2019Entities();
         public FormProduct()
         {
             InitializeComponent();
@@ -19,6 +20,30 @@ namespace projectpad
 
         private void FormProduct_Load(object sender, EventArgs e)
         {
+            refreshData();
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        public void refreshData()
+        {
+            var products = from Product p in db.Products
+                           select new
+                           {
+                               ProductName = p.Name,
+                               MakeFlag = p.MakeFlag,
+                               FinishedGoodsFlag = p.FinishedGoodsFlag,
+                               SafetyStockLevel = p.SafetyStockLevel,
+                               ReorderPoint = p.ReorderPoint,
+                               StandardCost = p.StandardCost,
+                               ListPrice = p.ListPrice,
+                               DaysToManufacture = p.DaysToManufacture,
+                               SellStartDate = p.SellStartDate,
+                           };
+            dataGridView1.DataSource = products.ToList();
 
         }
     }
